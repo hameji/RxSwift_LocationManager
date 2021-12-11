233553899a7c6d420131893b6871f5dfa2315e27
Hajime Taniguchi <hameji@gmail.com>
2021-12-10 21:31:03 -0500

    outletをviewcontrollerに接続

--- project/RxSwift+CLLocationManager/ViewController.swift
+++ project/RxSwift+CLLocationManager/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    
+    @IBOutlet weak var startGPSButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
